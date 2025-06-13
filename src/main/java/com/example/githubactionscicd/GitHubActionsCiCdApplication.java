dcca422c2cc9afb1c18a987769ac4c1e3af29fac
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-06-13 22:53:44 +0300

    Initial commit

---
+++ src/main/java/com/example/githubactionscicd/GitHubActionsCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsCiCdApplication.class, args);
+    }
+
+}
